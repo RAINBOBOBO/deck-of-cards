12e925e6eb4c3e9b97e11a31fa264c8472bfdda0
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-08-28 15:40:29 -0700

    enabled a shuffle deck button. Refactored useEffect to listen for changes in isShuffling and isDrawing states

--- src/CardTable.js
+++ src/CardTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { render } from '@testing-library/react';
 import Card from './Card';
+import uuid from 'uuid/v4';
 
 const NEW_DECK_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 
@@ -17,46 +18,64 @@ const NEW_DECK_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_coun
 function CardTable() {
   const [deckId, setDeckId] = useState(null);
   const [deck, setDeck] = useState([]);
-  const [cardCount, setCardCount] = useState(0);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(true);
 
-  // for getting deckId, only called once
-  useEffect(function getDeckIdWhenMounted() {
+  // for getting deckId
+  useEffect(function getDeckIdWhenRendered() {
     async function getDeckId() {
       const deckResult = await axios.get(NEW_DECK_URL);
+      setIsShuffling(false);
       setDeckId(deckResult.data.deck_id);
     }
-    getDeckId();
-  }, []);
+    if (isShuffling === true) {
+      getDeckId();
+    }
+  }, [isShuffling]);
 
   useEffect(function getCardFromDeck() {
     async function getCard() {
       const cardResult = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`)
-      const newCard = {image: cardResult.data.cards[0].image}
+      setIsDrawing(false);
+      const newCard = { image: cardResult.data.cards[0].image, key: uuid() }
       setDeck(deck => [...deck, newCard])
     }
-    getCard();
-  }, [cardCount]);
-
-  function increaseCount(evt) {
-    if (cardCount <= 52) {
-      setCardCount(c => c + 1);
+    if (isDrawing === true) {
+      getCard();
     }
+  }, [isDrawing]);
+
+  function drawCard(evt) {
+    setIsDrawing(true);
+  }
+
+
+  function shuffleDeck(evt) {
+    setIsShuffling(true);
+    setDeck([]);
   }
 
   function showButtonOrMsg() {
-    if (cardCount >= 52) {
+    if (deck.length >= 52) {
       return <p>NO MORE CARDS!</p>
     } else {
-      return <button onClick={increaseCount} >GIMME A CARD!</button>
+      return <button onClick={drawCard}>GIMME A CARD!</button>
     }
   }
 
+  // shuffle deck button
+  // if clicked -> change deckCont state
+  // trigger useEffect for getDeckId
+  // Reset deck and cardCount to initial values
 
   return (
     <div>
       {showButtonOrMsg()}
+      {!isShuffling &&
+        <button onClick={shuffleDeck}>Shuffle Cards</button>}
       {deck.map(
         card => (<Card
+          key={card.key}
           image={card.image}
         />)
       )}
